e89a09b5451f1791d9c25bd1272fd281bcd48fe1
eibmac20 <eibmac20@htl-kaindorf.at>
2024-05-03 09:22:15 +0200

    styles for login

--- src/App.tsx
+++ src/App.tsx
@@ -22,19 +22,20 @@ function App() {
 
     if (!session) {
         return (
-            <div>
-                <img src="/rss-41072_1280.png" alt="RSS Icon"/>
-                <h3>Welcome to Really-Sophisticated-Story-Feed!</h3>
-                <h4>Your personal RSS-feed</h4>
-                <hr/>
+            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-5">
+                <img src="/rss-41072_1280.png" alt="RSS Icon" className="w-32 h-32 mb-10"/>
+                <h3 className="text-2xl font-bold text-gray-900">Welcome to Really-Sophisticated-Story-Feed!</h3>
+                <h4 className="text-xl text-gray-700">Your personal RSS-feed</h4>
+                <hr className="my-4"/>
                 <Auth providers={[]} supabaseClient={supabase} appearance={{theme: ThemeSupa}}/>
             </div>
         )
     } else {
         return (
-            <div>
-                <p>Logged in!</p>
-                <button onClick={() => supabase.auth.signOut()}>Sign out</button>
+            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-5">
+                <img src="/rss-41072_1280.png" alt="RSS Icon" className="w-32 h-32 mb-10"/>
+                <p className="text-xl text-gray-700">Logged in!</p>
+                <button onClick={() => supabase.auth.signOut()} className="px-4 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-600">Sign out</button>
             </div>
         )
     }
