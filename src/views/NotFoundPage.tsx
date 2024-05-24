bba79f9eca894b57f8b77b62c7d839e971f44654
eibmac20 <eibmac20@htl-kaindorf.at>
2024-05-24 14:12:02 +0200

    cute rss 404

--- src/views/NotFoundPage.tsx
+++ src/views/NotFoundPage.tsx
@@ -1,8 +1,10 @@
-import { Link } from 'react-router-dom';
+import rss404Image from '../assets/rss404.jpg';
+import {Link} from "react-router-dom";
 
 const NotFoundPage = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+        <div className="flex flex-col items-center justify-center sm:mt-52 mt-5 text-center p-4">
+            <img src={rss404Image} alt="404 error Content credentials: Generated with AI ∙ 24 May 2024 at 2:05 pm" className="mb-4 h-52 rounded-xl"/>
             <h1 className="text-6xl font-bold text-red-600">404</h1>
             <p className="text-2xl">Oops! The page you are looking for does not exist.</p>
             <Link to="/" className="mt-6 text-blue-500 hover:underline">Go back to Home</Link>
