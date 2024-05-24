e720b0bf36b1fcbd20eb001092caaec2a5188935
eibmac20 <eibmac20@htl-kaindorf.at>
2024-05-24 12:58:49 +0200

    icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import supabase from "@/utils/supabase";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import rssImage from '../assets/rss-41072_1280.png';
+
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -12,22 +14,42 @@ const Header = () => {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-4">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-                <span className="font-semibold text-xl tracking-tight">RSSFeed</span>
+                <span className="font-semibold text-xl tracking-tight">
+                    <Link to="/">
+                        <img src={rssImage} alt="RSS logo" className="inline-block align-middle w-auto h-6 mr-2"/>
+                    </Link>
+                    RSSFeed
+                </span>
             </div>
             <div className="block lg:hidden">
-                <button onClick={toggleMenu} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                <button onClick={toggleMenu}
+                        className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>Menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+                    </svg>
                 </button>
             </div>
-            <div className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? '' : 'hidden'} lg:block`}>
+            <div
+                className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? '' : 'hidden'} lg:block`}>
                 <div className="text-sm lg:flex-grow">
-                    <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Home</Link>
-                    <Link to="/account" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">My account</Link>
-                    <Link to="/my-feeds" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">My feeds</Link>
-                    <Link to="/my-articles" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">My Articles</Link>
+                    <Link to="/"
+                          className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Home</Link>
+                    <Link to="/account"
+                          className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">My
+                        account</Link>
+                    <Link to="/my-feeds"
+                          className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">My
+                        feeds</Link>
+                    <Link to="/my-articles"
+                          className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">My
+                        Articles</Link>
                 </div>
                 <div>
-                    <button className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0" onClick={() => supabase.auth.signOut()}>Sign off</button>
+                    <button
+                        className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+                        onClick={() => supabase.auth.signOut()}>Sign off
+                    </button>
                 </div>
             </div>
         </nav>
