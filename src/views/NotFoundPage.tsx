26d9db0a26f7f015cb26ff94c21a7e4aec4c01cc
eibmac20 <eibmac20@htl-kaindorf.at>
2024-05-24 15:30:48 +0200

    http.cat Easter Egg

--- src/views/NotFoundPage.tsx
+++ src/views/NotFoundPage.tsx
@@ -5,8 +5,10 @@ const NotFoundPage = () => {
     return (
         <div className="flex flex-col items-center justify-center sm:mt-48 mt-5 text-center p-4">
             <div className="overflow-hidden h-52 w-auto rounded-xl mb-10">
-                <img src={rss404Image} alt="404 error Content credentials: Generated with AI ∙ 24 May 2024 at 2:05 pm"
-                     className="h-full w-full object-cover rounded-xl transform transition-all duration-300 hover:scale-110"/>
+                <a href="https://http.cat/status/404" target="_blank" rel="noopener noreferrer" style={{cursor: 'default'}}>
+                    <img src={rss404Image} alt="404 error Content credentials: Generated with AI ∙ 24 May 2024 at 2:05 pm"
+                         className="h-full w-full object-cover rounded-xl transform transition-all duration-300 hover:scale-110"/>
+                </a>
             </div>
             <h1 className="text-6xl font-bold text-red-600">404</h1>
             <p className="text-2xl">Oops! The page you are looking for does not exist.</p>
