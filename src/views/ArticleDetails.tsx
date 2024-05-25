494ae0ea14e992811453370b590c84bd15fc73b7
eibmac20 <eibmac20@htl-kaindorf.at>
2024-05-25 10:20:37 +0200

    now I fixed them

--- src/views/ArticleDetails.tsx
+++ src/views/ArticleDetails.tsx
@@ -42,7 +42,7 @@ const ArticleDetails = () => {
         }
 
         fetchArticle().then(() => console.log("fetchArticle executed"));
-    }, [toast]);
+    }, [toast, item_id]);
 
     return (
         <div className={"p-10"}>
