bedc05c9f7da816d01c1714ce8af642ecc66b192
Jonas Dietrich <diejoc20@htl-kaindorf.at>
2024-06-07 10:01:05 +0200

    added icons, spoolered some spool

--- src/views/MyArticles.tsx
+++ src/views/MyArticles.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchPaginatedArticles, fetchUserArticles } from "@/utils/requestHelper";
+import rssArticleLibrarian from "../assets/rssArticleLibrarian.webp"
+
 import ArticleList from "@/components/ArticleList";
 
 const MyArticles = () => {
@@ -26,7 +28,10 @@ const MyArticles = () => {
 
     return (
         <div>
-            <h1>My Feeds</h1>
+            <div className={`flex flex-col items-center justify-center mt-5 text-gray-900`}>
+                <img src={rssArticleLibrarian} alt="Our Rss Article Lirarian"
+                 content={"Content credentials: Generated with AI ∙ 4 June 2024 at 15:36 pm"} className="mb-4 size-72"/>
+            </div>
             <div>
                 <ArticleList articles={articles} loadMore={loadMore}/>
             </div>
--- src/views/MyFeeds.tsx
+++ src/views/MyFeeds.tsx
@@ -1,9 +1,13 @@
 import FeedList from "@/components/FeedList.tsx";
+import rssChanneller from "../assets/rssChannelManager.webp";
 
 const MyFeeds = () => {
     return (
         <div>
-            <h1>My Feeds</h1>
+            <div className={`flex flex-col items-center justify-center mt-5 text-gray-900`}>
+                <img src={rssChanneller} alt="Our Rss Article Lirarian"
+                 content={"Content credentials: Generated with AI ∙ 4 June 2024 at 15:36 pm"} className="mb-4 size-72"/>
+            </div>
             <div>
                 <FeedList/>
             </div>
