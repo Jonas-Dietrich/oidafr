45adf3dd3019e463f730dbffc6c344d2725d90d2
Jonas Dietrich <diejoc20@htl-kaindorf.at>
2024-06-20 10:09:41 +0200

    added loader

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import RssItemItem from "./RssItemItem";
 import { Button } from "./ui/button";
-import { fetchPaginatedArticles, fetchPaginatedArticlesByUrl} from "@/utils/requestHelper";
+import { fetchPaginatedArticles, } from "@/utils/requestHelper";
+import Loading from "@/components/Loading.tsx";
 
 
 /**
@@ -10,27 +11,29 @@ import { fetchPaginatedArticles, fetchPaginatedArticlesByUrl} from "@/utils/requ
  * Date: 23. 5. 2024
  */
 
-interface ArticleListProps {
-    articleUrls: string[], 
-}
 
-const ArticleList:React.FC<ArticleListProps> = ({articleUrls}) => {
+const ArticleList = () => {
 
     const [articles, setArticles] = useState<RssItem[]>([]);
     const [currentPage, setCurrentPage] = useState<number>(0);
     const [buttonActive, setButtonActive] = useState<boolean>(true);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     useEffect(() => {
-        fetchPaginatedArticlesByUrl(articleUrls, currentPage, 20).then((data:ItemPageable) => setArticles(data.content));
+        fetchPaginatedArticles(currentPage, 20).then((data:ItemPageable) => setArticles(data.content));
     }, []);
 
     const loadMore = () => {
+        console.log(isLoading);
         setCurrentPage(currentPage + 1);
 
         fetchPaginatedArticles(currentPage, 20).then((data:ItemPageable) => {
             setArticles([...articles, ...data.content]);
 
             if (currentPage + 1 >= data.pageable.totalPages) setButtonActive(false);
+            setIsLoading(false);
+        }).catch(() => {
+            setIsLoading(false);
         })
     }
     
@@ -42,8 +45,9 @@ const ArticleList:React.FC<ArticleListProps> = ({articleUrls}) => {
             )
         }
 
-        <div className="flex flex-col items-center justify-center">
-            { buttonActive ? <Button variant="default" onClick={loadMore}>Load More</Button> : <></> }
+        <div className="flex flex-col items-center justify-center mb-4">
+            {isLoading ? <div className="m-5"><Loading/></div> : <></>}
+            { buttonActive ? <Button variant="default" onClick={() => {loadMore(); setIsLoading(true)}}>Load More</Button> : <></> }
         </div>
     </div>);
 }
--- src/views/MyArticles.tsx
+++ src/views/MyArticles.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
 import rssArticleLibrarian from "../assets/rssArticleLibrarian.webp"
-
 import ArticleList from "@/components/ArticleList";
-import { fetchUserFeeds } from "@/utils/requestHelper";
 
 /**
  * Project: RssFrontend
@@ -10,13 +7,6 @@ import { fetchUserFeeds } from "@/utils/requestHelper";
  * Date: 16.05.24
  */
 const MyArticles = () => {
-
-    const [articleUrls, setArticleUrls] = useState<string[]>([]);
-
-    useEffect(() => {
-        fetchUserFeeds().then(setArticleUrls);
-    }, []);
-
     return (
         <div>
             <div className={`flex flex-col items-center justify-center mt-5 text-gray-900`}>
@@ -24,7 +14,7 @@ const MyArticles = () => {
                  content={"Content credentials: Generated with AI ∙ 4 June 2024 at 15:36 pm"} className="mb-4 size-72"/>
             </div>
             <div>
-                <ArticleList articleUrls={articleUrls}/>
+                <ArticleList />
             </div>
         </div>
     );
